0b243d390a8f35bf0a19e14d47215f309852533a
junwoo1519 <junwoo101797@gmail.com>
2022-06-30 23:40:29 +0900

    input에 ID를 추가해 값을 가져오고 console로 출력 되는지 확인

---
+++ js/create.js
@@ -0,0 +1,8 @@
+function setCreate(){
+  let titleData = document.getElementById("title").value;
+  let contentData = document.getElementById("content").value;
+  let writerData = document.getElementById("writer").value;
+  console.log(titleData);
+  console.log(contentData);
+  console.log(writerData);
+}
